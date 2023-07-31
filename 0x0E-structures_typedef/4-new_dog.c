ba57ba2c540bd983105dcadc908c40c22b10bccc
UsmanAmeerMuhammad <ameerusman091@gmail.com>
2023-07-31 12:10:36 +0000

    betty error

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -15,11 +15,11 @@ dog_t *new_dog(char *name, float age, char *owner)
 {
 	dog_t *new_dog;
 	int len_name, len_owner;
-	
+
 	new_dog = malloc(sizeof(dog_t));
 	if (new_dog == NULL)
 		return (NULL);
-	
+
 	len_name = strlen(name);
 	new_dog->name = malloc(sizeof(char) * len_name + 1);
 	if (new_dog->name == NULL)
@@ -36,9 +36,9 @@ dog_t *new_dog(char *name, float age, char *owner)
 		free(new_dog);
 		return (NULL);
 	}
-	
+
 	new_dog->owner = strcpy(new_dog->owner, owner);
 	new_dog->age = age;
-	
+
 	return (new_dog);
 }
